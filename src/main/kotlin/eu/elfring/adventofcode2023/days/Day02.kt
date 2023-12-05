f39482c6f91d8fb2ac82b6769e39639745887c08
Tom Elfring <tom.elfring@topicus.nl>
2023-12-05 13:04:09 +0100

    Day 2: Cube Conundrum

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day02.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day02.kt
@@ -6,11 +6,47 @@ class Day02 : AoCPuzzle(2)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val maxRedCubes = 12
+        val maxGreenCubes = 13
+        val maxBlueCubes = 14
+
+        val mapped = input.map(::parseLine)
+
+        return mapped.sumOf { if (it.second.all { it.isValid(maxRedCubes, maxGreenCubes, maxBlueCubes) }) it.first else 0 }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val mapped = input.map(::parseLine)
+
+        return mapped.sumOf {
+            val minReds = it.second.maxOf { it.red }
+            val minGreens = it.second.maxOf { it.green }
+            val minBlues = it.second.maxOf { it.blue }
+
+            minReds*minGreens*minBlues
+        }
+    }
+
+    private fun parseLine(line: String): Pair<Int, List<Revealed>>
+    {
+        val (game, drawn) = line.split(":")
+
+        val revealed = drawn.split(";").map {
+            val reds = """(\d+) red""".toRegex().find(it)?.groupValues?.get(1)?.toInt() ?: 0
+            val greens = """(\d+) green""".toRegex().find(it)?.groupValues?.get(1)?.toInt() ?: 0
+            val blues = """(\d+) blue""".toRegex().find(it)?.groupValues?.get(1)?.toInt() ?: 0
+            Revealed(reds, greens, blues)
+        }
+
+        return game.filter { it.isDigit() }.toInt() to revealed
+    }
+
+    private data class Revealed(val red: Int, val green: Int, val blue: Int)
+    {
+        fun isValid(maxRed: Int, maxGreen: Int, maxBlue: Int): Boolean
+        {
+            return red<=maxRed && green<=maxGreen && blue<=maxBlue
+        }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2023/Day02Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2023/Day02Test.kt
@@ -10,20 +10,24 @@ import org.junit.jupiter.api.Test
 internal class Day02Test
 {
     private val input = """
-        TODO INPUT
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day02().part1(input))
+        Assertions.assertEquals(8, Day02().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day02().part2(input))
+        Assertions.assertEquals(2286, Day02().part2(input))
     }
 }
