448d69934106e61e32dc9792ecdf56104ee44d9a
Tom <1036527+tomelfring@users.noreply.github.com>
2023-12-05 13:13:49 +0100

    Day 5: If You Give A Seed A Fertilizer (#5)

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day05.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day05.kt
@@ -6,11 +6,130 @@ class Day05 : AoCPuzzle(5)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val seeds = input.first().split(" ").drop(1).map { it.toLong() }
+        val (seedsToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation) =
+            input.drop(2).joinToString("\n").split("\n\n").map { it.split("\n").drop(1).map { it.split(" ").map { it.toLong() } }.map(::convertToCalculation) }
+
+        val locations = seeds.map { seed ->
+            val soil = seedsToSoil.firstOrNull { seed in it.range }?.addition?.let { seed+it } ?: seed
+            val fertilizer = soilToFertilizer.firstOrNull { soil in it.range }?.addition?.let { soil+it } ?: soil
+            val water = fertilizerToWater.firstOrNull { fertilizer in it.range }?.addition?.let { fertilizer+it } ?: fertilizer
+            val light = waterToLight.firstOrNull { water in it.range }?.addition?.let { water+it } ?: water
+            val temperature = lightToTemperature.firstOrNull { light in it.range }?.addition?.let { light+it } ?: light
+            val humidity = temperatureToHumidity.firstOrNull { temperature in it.range }?.addition?.let { temperature+it } ?: temperature
+            humidityToLocation.firstOrNull { humidity in it.range }?.addition?.let { humidity+it } ?: humidity
+        }
+
+        return locations.min()
+    }
+
+    private fun LongRange.intersectToRange(range: LongRange): Pair<LongRange?, List<LongRange>> {
+        // If there is a part in front
+        val front = if (this.first < range.first)
+        {
+            this.first.until(kotlin.math.min(this.last + 1, range.first))
+        }
+        else null
+
+        // intersection itself
+        val min = (kotlin.math.max(this.first, range.first))
+        val max = (kotlin.math.min(this.last, range.last))
+        val intersection = if (min < max) min..max else null
+
+        // after
+        val end = if (this.last > range.last)
+        {
+            (kotlin.math.max(this.first, range.last + 1))..this.last
+        }
+        else null
+
+        return intersection to listOfNotNull(front, end)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val seedsInput = input.first().split(" ").drop(1).map { it.toLong() }
+        val seeds = seedsInput.chunked(2).map { it[0]..it[0]+it[1] }
+
+        val (seedsToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation) =
+            input.drop(2).joinToString("\n").split("\n\n").map { it.split("\n").drop(1).map { it.split(" ").map { it.toLong() } }.map(::convertToCalculation) }
+
+        var matched = mutableListOf<LongRange>()
+        var unmatched = seeds
+        seedsToSoil.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        soilToFertilizer.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        fertilizerToWater.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        waterToLight.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        lightToTemperature.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        temperatureToHumidity.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+        unmatched+=matched
+        matched = mutableListOf()
+        humidityToLocation.forEach { calculation ->
+            unmatched = unmatched.flatMap {
+                val (intersect, other) = it.intersectToRange(calculation.range)
+                intersect?.let { matched.add((intersect.first+calculation.addition)..(intersect.last+calculation.addition)) }
+                other
+            }
+        }
+
+        return (matched+unmatched).minOf { it.min() }
     }
+
+    private fun convertToCalculation(input: List<Long>): Calculation
+    {
+        val (destinationRangeStart, sourceRangeStart, rangeLength) = input
+        return Calculation(sourceRangeStart..sourceRangeStart+rangeLength, destinationRangeStart-sourceRangeStart)
+    }
+
+    private data class Calculation(val range: LongRange, val addition: Long)
 }
+
+private operator fun <T> List<T>.component6() = get(5)
+private operator fun <T> List<T>.component7() = get(6)
+private operator fun <T> List<T>.component8() = get(7)
--- src/test/kotlin/eu/elfring/adventofcode2023/Day05Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2023/Day05Test.kt
@@ -10,20 +10,52 @@ import org.junit.jupiter.api.Test
 internal class Day05Test
 {
     private val input = """
-        TODO INPUT
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day05().part1(input))
+        Assertions.assertEquals(35L, Day05().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day05().part2(input))
+        Assertions.assertEquals(46L, Day05().part2(input))
     }
 }
