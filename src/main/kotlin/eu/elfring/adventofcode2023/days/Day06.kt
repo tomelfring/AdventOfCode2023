f9dab1b88438ea0e94a3c35a231f593b219f1fbe
Tom <1036527+tomelfring@users.noreply.github.com>
2023-12-06 09:59:28 +0100

    Day 6: Wait For It (#6)

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day06.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day06.kt
@@ -6,11 +6,32 @@ class Day06 : AoCPuzzle(6)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val digitRegex = """(\d+)""".toRegex()
+        val times = digitRegex.findAll(input[0]).map { it.value.toLong() }.toList()
+        val distances = digitRegex.findAll(input[1]).map { it.value.toLong() }.toList()
+
+        val pairs = times.zip(distances)
+
+        return pairs.map(::calculateTimesWithEnoughDistance).reduce { acc, value -> acc*value }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val time = input[0].filter { it.isDigit() }.toLong()
+        val distance = input[1].filter { it.isDigit() }.toLong()
+
+        return calculateTimesWithEnoughDistance(time to distance)
+    }
+
+    private fun calculateTimesWithEnoughDistance(pair: Pair<Long, Long>): Int
+    {
+        val (time, distanceNeeded) = pair
+
+        return (0L..time).count { timePressed ->
+            val timeToRace = time-timePressed
+            val speed = timePressed
+
+            speed*timeToRace>distanceNeeded
+        }
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2023/Day06Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2023/Day06Test.kt
@@ -10,20 +10,21 @@ import org.junit.jupiter.api.Test
 internal class Day06Test
 {
     private val input = """
-        TODO INPUT
+        Time:      7  15   30
+        Distance:  9  40  200
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day06().part1(input))
+        Assertions.assertEquals(288, Day06().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day06().part2(input))
+        Assertions.assertEquals(71503, Day06().part2(input))
     }
 }
