96b3e2501f394baa1c9fe72d89b6a39005abdf5f
Tom Elfring <tom.elfring@topicus.nl>
2023-12-08 14:48:13 +0100

    Day 8: Haunted Wasteland

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day08.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day08.kt
@@ -1,16 +1,88 @@
 package eu.elfring.adventofcode2023.days
 
 import eu.elfring.adventofcode2023.AoCPuzzle
+import java.math.BigInteger
 
 class Day08 : AoCPuzzle(8)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val instructions = input.first()
+        val network = input.drop(2).associate { toNodes(it) }
+
+        // solve
+        var currentPos = "AAA"
+        var steps = 0
+        while (currentPos != "ZZZ")
+        {
+            val instruction = instructions[steps%instructions.length]
+            currentPos = if (instruction == 'L') network[currentPos]!!.first else network[currentPos]!!.second
+
+            steps++
+        }
+
+        return steps
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val instructions = input.first()
+        val network = input.drop(2).associate { toNodes(it) }
+
+        // solve
+        var currentPositions = listOf(network.filterKeys { it.endsWith("A") }).flatMap { it.keys }
+
+        var steps = 0
+
+        // Instruction index, when Z was found
+        val loopDetection = mutableMapOf<Int, Int>()
+        // Instruction index, first Z + stepsize
+        val loops = mutableMapOf<Int, Int>()
+
+        while (loops.size != currentPositions.size)
+        {
+            val instruction = instructions[steps%instructions.length]
+
+            currentPositions = currentPositions.map {
+                if (instruction == 'L') network[it]!!.first else network[it]!!.second
+            }
+
+            currentPositions.forEachIndexed { index, s ->
+                if (s.endsWith("Z"))
+                {
+                    // First time reaching Z
+                    if (loopDetection[index] == null)
+                    {
+                        loopDetection[index] = steps
+                    }
+                    // Second time reaching Z, calculate offset & roundtrip steps
+                    else if (loops[index] == null)
+                    {
+                        loops[index] = steps-loopDetection[index]!!
+                    }
+                }
+            }
+
+            steps++
+        }
+
+        println(loops)
+
+        val gcd = loops.values.reduce { acc, i -> gcd(acc, i) }
+
+        val lcm = loops.values.map { it.toLong() }.reduce { acc, i -> acc*(i/gcd) }
+
+        return lcm
+    }
+
+    private fun toNodes(rule: String): Pair<String, Pair<String, String>>
+    {
+        val (from, left, right) = """([A-Z0-9]{3})""".toRegex().findAll(rule).map { it.value }.toList()
+        return from to (left to right)
+    }
+
+    private fun gcd(x: Int, y: Int): Int
+    {
+        return if (y == 0) x else gcd(y, x%y)
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2023/Day08Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2023/Day08Test.kt
@@ -10,20 +10,37 @@ import org.junit.jupiter.api.Test
 internal class Day08Test
 {
     private val input = """
-        TODO INPUT
+        LLR
+
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day08().part1(input))
+        Assertions.assertEquals(6, Day08().part1(input))
     }
+
+    private val input2 = """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+    """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day08().part2(input))
+        Assertions.assertEquals(6L, Day08().part2(input2))
     }
 }
