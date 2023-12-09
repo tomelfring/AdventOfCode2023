1ec8989cd7f285837e4796775fe18bf923273c1b
Tom <1036527+tomelfring@users.noreply.github.com>
2023-12-09 09:34:36 +0100

    Day 9: Mirage Maintenance (#9)

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day09.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day09.kt
@@ -6,11 +6,34 @@ class Day09 : AoCPuzzle(9)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        return input.sumOf { extrapolate(it, true) }
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        return input.sumOf { extrapolate(it, false) }
+    }
+
+    private fun extrapolate(input: String, right: Boolean): Int
+    {
+        val numbers = input.split(" ").map { it.toInt() }
+
+        val history = mutableListOf(numbers)
+        var diff = numbers.zipWithNext().map { it.second - it.first }
+        while (!diff.all { it == 0 })
+        {
+            history.add(diff)
+            diff = history.last().zipWithNext().map { it.second - it.first }
+        }
+        history.add(diff)
+
+        return if (right)
+        {
+            history.sumOf { it.last() }
+        }
+        else
+        {
+            history.map { it.first() }.reduceRight { i, acc -> i-acc }
+        }
     }
 }
