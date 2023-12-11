4a458f46dc5357dd56c2eec206cfbaa14e8350f3
Tom Elfring <tom.elfring@topicus.nl>
2023-12-11 10:23:30 +0100

    Day 11: Cosmic Expansion

--- src/main/kotlin/eu/elfring/adventofcode2023/days/Day11.kt
+++ src/main/kotlin/eu/elfring/adventofcode2023/days/Day11.kt
@@ -1,16 +1,79 @@
 package eu.elfring.adventofcode2023.days
 
 import eu.elfring.adventofcode2023.AoCPuzzle
+import kotlin.math.abs
+import kotlin.math.exp
 
 class Day11 : AoCPuzzle(11)
 {
     override fun part1(input: List<String>): Any
     {
-        return 0
+        val galaxies = getGalaxies(input)
+
+        val expandedGalaxies = galaxies.map { it.withExpansion(1) }
+
+        return getDistances(expandedGalaxies)
     }
 
     override fun part2(input: List<String>): Any
     {
-        return 0
+        val galaxies = getGalaxies(input)
+
+        val expandedGalaxies = galaxies.map { it.withExpansion(999_999) }
+
+       return getDistances(expandedGalaxies)
+    }
+
+    private fun getGalaxies(input: List<String>): List<Galaxy>
+    {
+        // Calculate empty rows & columns
+        val emptyRows = input.map { row -> row.all { it == '.' } }
+        val emptyColumns = input.first().indices.map { column ->
+            input.all { it[column] == '.' }
+        }
+
+        var tempOffset = 0
+        val rowOffsets = emptyRows.map { if (it) ++tempOffset else tempOffset }
+        tempOffset = 0
+        val columnOffsets = emptyColumns.map { if (it) ++tempOffset else tempOffset }
+
+        return buildList {
+            input.forEachIndexed { y, row ->
+                row.forEachIndexed { x, c ->
+                    if (c == '#')
+                    {
+                        this.add(Galaxy(Point(x.toLong(), y.toLong()), columnOffsets[x], rowOffsets[y]))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getDistances(galaxies: List<Galaxy>): Long
+    {
+        val galaxyPairs = galaxies.map { it to galaxies-it }
+
+        return galaxyPairs.sumOf {
+            val (from, tos) = it
+            tos.sumOf { it.position.distanceTo(from.position) }
+        } / 2
+    }
+
+    private data class Galaxy(
+        val position: Point,
+        val xOffset: Int,
+        val yOffset: Int
+    )
+    {
+        fun withExpansion(cosmicExpansionFactor: Int) = Galaxy(this.position.withOffset(xOffset, yOffset, cosmicExpansionFactor), xOffset, yOffset)
+    }
+
+    private data class Point(
+        val x: Long,
+        val y: Long
+    )
+    {
+        fun distanceTo(other: Point) = abs(other.x-this.x) + abs(other.y-this.y)
+        fun withOffset(xOffset: Int, yOffset: Int, cosmicExpansionFactor: Int) = Point(x+xOffset*cosmicExpansionFactor, y+yOffset*cosmicExpansionFactor)
     }
 }
--- src/test/kotlin/eu/elfring/adventofcode2023/Day11Test.kt
+++ src/test/kotlin/eu/elfring/adventofcode2023/Day11Test.kt
@@ -10,20 +10,29 @@ import org.junit.jupiter.api.Test
 internal class Day11Test
 {
     private val input = """
-        TODO INPUT
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
     """.trimIndent().split("\n")
 
     @Test
     @DisplayName("Part 1")
     fun part1()
     {
-        Assertions.assertEquals(0, Day11().part1(input))
+        Assertions.assertEquals(374L, Day11().part1(input))
     }
 
     @Test
     @DisplayName("Part 2")
     fun part2()
     {
-        Assertions.assertEquals(0, Day11().part2(input))
+        Assertions.assertEquals(82000210L, Day11().part2(input))
     }
 }
